bfbaaa89ea0f6929467f53f3e00ff7c69b0ea9b9
schettn <nicoschett@icloud.com>
2020-04-04 16:52:10 +0200

    Implement SnekGqlTemplate interface
    A interface for SnekGqlTemplate is implemented now.

--- src/templates/snek/gql/index.ts
+++ src/templates/snek/gql/index.ts
@@ -1,6 +1,14 @@
 import { SnekGqlQuery } from "./queries/index";
 import { SnekGqlMutation } from './mutations/index';
 
+export interface ISnekGqlTemplate {
+    queries: SnekGqlQuery;
+    mutations: SnekGqlMutation;
+}
+
+/**
+ * @description A Template with initializes all SubTemplates
+ */
 export class SnekGql {
     public queries = new SnekGqlQuery();
     public mutations = new SnekGqlMutation();
