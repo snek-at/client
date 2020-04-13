632f083194a0e1b6b14d1410dcfa681c8e4e07a9
sxct <nicoschett@icloud.com>
2020-04-13 11:58:56 -0700

    Replace cookie expire with max age
    The cookie expire is repalced with max age now.

--- src/session/cookie-utils.ts
+++ src/session/cookie-utils.ts
@@ -10,11 +10,10 @@ export function setCookie(name: string, val: string, time: number) {
     const date = new Date();
     const value = val;
 
-    // Set it expire in 30 seconds
+    // Set it expire in n(time) seconds
     date.setTime(date.getTime() + (time * 1000));
-
     // Set it
-    document.cookie = name + "=" + value + "; expires=" + date.toUTCString() + "; path=/";
+    document.cookie = name + "=" + value + "; max-age=" + date.toUTCString() + "; path=/";
 }
 
 /**
@@ -42,7 +41,7 @@ export function deleteCookie(name: string) {
     date.setTime(date.getTime() + (-1 * 24 * 60 * 60 * 1000));
 
     // Set it
-    document.cookie = name + "=; expires=" + date.toUTCString() + "; path=/";
+    document.cookie = name + "=; max-age=" + date.toUTCString() + "; path=/";
 }
 
 /**
