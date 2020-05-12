1c7d4782588bd5bb07c5304332f67f535cf54b30
sxct <nicoschett@icloud.com>
2020-05-12 11:29:53 +0200

    Improve code quality
    The code quality has been improved due to the codacy check.

--- src/session/cookie-utils.ts
+++ src/session/cookie-utils.ts
@@ -30,7 +30,7 @@ function getCookie(name: string) {
   const value = "; " + document.cookie;
   const parts = value.split("; " + name + "=");
 
-  if (parts.length == 2) {
+  if (parts.length === 2) {
     return parts.pop()?.split(";").shift();
   }
 }
