c9264c8826acc6e959459a6e14ccc811f5e477c7
schettn <nicoschett@icloud.com>
2020-05-13 13:00:20 +0200

    Add proxy
    The proxy has been added to the scraper.

--- src/endpoints/scraper.ts
+++ src/endpoints/scraper.ts
@@ -13,6 +13,7 @@ class Scraper implements ScraperEndpoint {
     accept: "application/json, text/plain, */*",
   };
   desc: string = "A endpoint to fetch page DOM";
+  proxy: string = "https://cors.snek.at/";
 
   /**
    * @constructor
@@ -26,6 +27,15 @@ class Scraper implements ScraperEndpoint {
     this.headers = { ...this.headers, ...options.headers };
   }
 
+  //> Getter
+  /**
+   * @returns {string} A URL
+   * @description Provides a URL which is composed of proxy and root url
+   */
+  get url(): string {
+    return this.proxy + this.root;
+  }
+
   //> Methods
   /**
    * @param path Path to the endpoint. Specify it like "/foo/bar". The correct placement of the slashes is essential!
@@ -33,11 +43,11 @@ class Scraper implements ScraperEndpoint {
    * @description Get JSON object<T> from specified path.
    */
   async getJson<T>(path: string): Promise<T> {
-    return fetch(this.root + path, {
+    return fetch(this.url + path, {
       headers: {
         ...this.headers,
       },
-    }).then((response) => {
+    }).then(async (response) => {
       if (!response.ok) {
         throw new Error(response.statusText);
       }
@@ -52,7 +62,7 @@ class Scraper implements ScraperEndpoint {
    * @description Get DOM object from specified path.
    */
   async getDom(path: string): Promise<Document> {
-    return fetch(this.root + path, {
+    return fetch(this.url + path, {
       headers: {
         ...this.headers,
       },
